a37dcc83f0f6a60d8303999080662d7c461044fd
abda616 <73405694+abda616@users.noreply.github.com>
2024-07-11 11:15:51 +0300

    Update Lambda Code.py

--- Lambda Code.py
+++ Lambda Code.py
@@ -1,13 +1,15 @@
+import os
 import boto3
 import json
 from datetime import datetime
 
-session = boto3.Session(region_name='us-east-1')
-required_tags = {"dlm:managed": "true"}
-launch_template_id_is = 'lt-06c85cce92beb2a6b'  # Replace with your Launch Template ID
+session = boto3.Session(region_name = os.environ['region_name_is'])         # map to region
+required_tags = {"aws:dlm:lifecycle-policy-id": os.environ['policy_id_is']} # map to policy_id
+launch_template_id_is = os.environ['launch_template_id_is']                 # map to lanch_template
 
 
-def has_tags(element, tags):
+
+def has_tags(element, tags):                            # check if described-images has the desired tags
     element_tags = {tag['Key']: tag['Value'] for tag in element.get('Tags', [])}
     for key, value in tags.items():
         if element_tags.get(key) != value:
@@ -15,56 +17,56 @@ def has_tags(element, tags):
     return True
 
 
-def parse_date(item):
+def parse_date(item):                                   # to check date formate it to be understand by 
     return datetime.fromisoformat(item["CreationDate"].replace("Z", "+00:00"))
 
 
 def get_latest_ami_id():
     ec2_client = session.client('ec2')
-    response = ec2_client.describe_images(
+    response = ec2_client.describe_images(                              # get AMI images
         Owners=['self']
     )
     data = response['Images']
-    data = [element for element in data if has_tags(element, required_tags)]
-    images = sorted(data, key=parse_date, reverse=True)
-    return images[0]['ImageId'] if images else None
+    data = [element for element in data if has_tags(element, required_tags)]    # data without desiered tags will be removed 
+    images = sorted(data, key=parse_date, reverse=True)                         # order the data based on time and reverse to find the most recent date
+    return images[0]['ImageId'] if images else None                         
 
 
 def create_launch_template_version(latest_ami_id, launch_template_id):
     ec2_client = session.client('ec2')
-    describe_response = ec2_client.describe_launch_templates(
+    describe_response = ec2_client.describe_launch_templates(   # get info about LT so I can find LatestVersionNumber -1->
         LaunchTemplateIds=[launch_template_id]
-    )
-    response = ec2_client.create_launch_template_version(
-        LaunchTemplateId=launch_template_id,
-        SourceVersion=str(describe_response['LaunchTemplates'][0]['LatestVersionNumber']),
+    )                                                   
+    response = ec2_client.create_launch_template_version(           #create new version of LT
+        LaunchTemplateId=launch_template_id,        
+        SourceVersion=str(describe_response['LaunchTemplates'][0]['LatestVersionNumber']),  # -2->  to update it in next LT version
         LaunchTemplateData={
-            'ImageId': latest_ami_id
+            'ImageId': latest_ami_id            # spicify AMI ID
         }
     )
-    return response['LaunchTemplateVersion']['VersionNumber']
+    return response['LaunchTemplateVersion']['VersionNumber'] #retern VersionNumber of new LT
 
 
 def update_launch_template(latest_ami_id, launch_template_id):
     ec2_client = session.client('ec2')
-    response = ec2_client.modify_launch_template(
-        LaunchTemplateId=launch_template_id,
-        DefaultVersion=str(create_launch_template_version(latest_ami_id, launch_template_id))
+    response = ec2_client.modify_launch_template(           #update LT VersionN 
+        LaunchTemplateId=launch_template_id,            
+        DefaultVersion=str(create_launch_template_version(latest_ami_id, launch_template_id))  #update
     )
     return response
 
 
 def lambda_handler(event, context):
-    latest_ami_id = get_latest_ami_id()
+    latest_ami_id = get_latest_ami_id()         # get ami if there is no ami ; Out else Countinue 
     if not latest_ami_id:
         return {
             'statusCode': 500,
             'body': json.dumps('No AMI found')
         }
-    launch_template_id = launch_template_id_is
-    response = update_launch_template(latest_ami_id, launch_template_id)
+    
+                                                # start update LT and 
+    response = update_launch_template(latest_ami_id, launch_template_id_is) 
     return {
         'statusCode': 200,
-        'body': json.dumps(
-            f'Launch template updated with AMI ID {latest_ami_id},version is {response["LaunchTemplate"]["LatestVersionNumber"]}')
+        'body': json.dumps(f'Launch template updated with AMI ID {latest_ami_id},version is {response["LaunchTemplate"]["LatestVersionNumber"]}')
     }
